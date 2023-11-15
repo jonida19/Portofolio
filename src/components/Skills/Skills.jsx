b90547acab09731a65a4dce2f47fe23640a0ccb3
Jonida Durbaku <durbaku.j@gmail.com>
2023-11-15 10:33:59 -0800

    added the About section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,18 +1,42 @@
-import React from 'react'
-import './skills.css'
-import UIDesign from '../../assets/ui_ux.png'
-
+import React from "react";
+import "./skills.css";
+import UIDesign from "../../assets/ui_ux.png";
+import WebDesign from "../../assets/web_design.PNG";
+import FullStack from "../../assets/full_stack.png";
 
 const Skills = () => {
   return (
-   <section id="skills">
-    <span className="skillTitle">What I do</span>
-    <span className="skillDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
-    <ul>
-        
-    </ul>
-   </section>
-  )
-}
+    <section id="skills">
+      <span className="skillTitle">What I do</span>
+      <span className="skillDesc">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </span>
+      <div className="skillBars">
+        <div className="skillBar">
+          <img src={UIDesign} alt="ui/ux design" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>UI/UX Design</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+        <div className="skillBar">
+          <img src={WebDesign} alt="Website Design" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>Website Design</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+        <div className="skillBar">
+          <img src={FullStack} alt="Full Stack" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>Full Stack Developement</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Skills
+export default Skills;
