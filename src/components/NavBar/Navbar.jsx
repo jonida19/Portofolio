78a1d22794f1bcf9f234a4f1a93f88ca4ad73bab
Jonida Durbaku <durbaku.j@gmail.com>
2023-11-16 12:02:03 -0800

    added the three lines menu on navbar for small screens

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -3,21 +3,34 @@ import "./navbar.css";
 import logo from '../../assets/jd_logo.png';
 import {Link} from 'react-scroll';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from "react";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <nav className="navbar">
       <img src={logo} alt="Logo" className="logo"/>
       <div className="desktopMenu">
-        <Link className="desktopMenuListItem">Home</Link>
-        <Link className="desktopMenuListItem">About</Link>
-        <Link className="desktopMenuListItem">Portofolio</Link>
-        <Link className="desktopMenuListItem">Blog</Link>
+        <Link activeClass="active" to="intro" spy={true} smooth={true} offset={-100} duration={500}  className="desktopMenuListItem">Home</Link>
+        <Link activeClass="active" to="skills" spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">About</Link>
+        <Link activeClass="active" to="works" spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Portofolio</Link>
+        <Link activeClass="active" to="contactPage" spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Blog</Link>
       </div>
-      <button className="desktopMenuButton">
+      <button className="desktopMenuButton" onClick={() =>
+      document.getElementById("contactPage").scrollIntoView()}>
         <ChatBubbleOutlineIcon/>
         &nbsp; Contact Me
       </button>
+
+      <MenuIcon className="mobMenu" onClick={()=> setShowMenu(!showMenu)}/>
+      <div className="navMenu" style={{display: showMenu? "flex" : "none"}}>
+        <Link activeClass="active" to="intro" spy={true} smooth={true} offset={-100} duration={500}  className="listItem"  onClick={() => setShowMenu= (false)}>Home</Link>
+        <Link activeClass="active" to="skills" spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={() => setShowMenu= (false)}>About</Link>
+        <Link activeClass="active" to="works" spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={() => setShowMenu= (false)}>Portofolio</Link>
+        <Link activeClass="active" to="contactPage" spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={() => setShowMenu= (false)}>Blog</Link>
+        <Link activeClass="active" to="contactPage" spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={() => setShowMenu= (false)}>Contact Me</Link>
+      </div>
     </nav>
   );
 };
