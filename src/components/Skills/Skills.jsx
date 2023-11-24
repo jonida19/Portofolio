e3f36766946c237c1f94ebe26d338260069b45d1
Jonida Durbaku <durbaku.j@gmail.com>
2023-11-24 12:18:46 -0800

    fixing the deployment issue

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./skills.css";
 import UIDesign from "../../assets/ux-ui.png";
-import WebDesign from "../../assets/web_design.png";
+import WebDesign from "../../assets/full_stack.png";
 import FullStack from "../../assets/full_stack.png";
 
 const Skills = () => {
